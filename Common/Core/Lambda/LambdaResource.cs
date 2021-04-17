88f7e4cd1aab16cccb7aa581b31bf1fdfcc7ddf1
David Colclazier <david@jdc.tech>
2021-04-17 11:31:51 -0600

    Tinkering... debugging

--- Common/Core/Lambda/LambdaResource.cs
+++ Common/Core/Lambda/LambdaResource.cs
@@ -8,16 +8,21 @@ namespace SuperNova.DiscordBot.Common.Core.Lambda
     public class LoggingResource
     {
         [Import]
-        private IServiceLoggerFactory _logFactory { get; set; } = null;
+        private IServiceLoggerFactory LogFactory { get; set; } = null;
         private readonly string _name;
         protected ILogger Logger { get; }
 
+        public LoggingResource()
+        {
+            MEFLoader.SatisfyImportsOnce(this);
+            _name = "LoggingResource";
+        }
         protected LoggingResource(string name)
         {
             MEFLoader.SatisfyImportsOnce(this);
 
             _name = name;
-            Logger = _logFactory.GetLogger(_name);
+            Logger = LogFactory.GetLogger(_name);
             Logger.LogInformation(_name);
         }
     }
